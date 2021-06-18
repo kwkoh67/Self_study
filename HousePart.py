de3d88ce98a8318552a35c83100fab957c9ebac4
kwkoh67 <kwkoh67@gmail.com>
2021-06-18 10:32:42 +0900

    Create HousePart.py
    HousePark

---
+++ HousePart.py
@@ -0,0 +1,14 @@
+﻿# park's house class 박씨집
+
+#class name : House park , last name, fullname 
+
+class HousePark:
+	lastname = "Park"
+	def setName(self, name):
+		self.fullname = name + self.lastname
+	
+pey = HousePark()
+pey.setName("KK")
+
+print(pey.fullname)
+	
