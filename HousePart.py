25a480487df54f53d7d9305bdd94d0c7cd704951
kwkoh67 <kwkoh67@gmail.com>
2021-06-18 10:45:44 +0900

    Update HousePart.py
    Python class 연습

--- HousePart.py
+++ HousePart.py
@@ -1,14 +1,28 @@
-﻿# park's house class 박씨집
+﻿#park's house class 박씨집
 
-#class name : House park , last name, fullname 
+#class HousePark, 이름 생성, 여행가고싶은곳
 
 class HousePark:
-	lastname = "Park"
-	def setName(self, name):
-		self.fullname = name + self.lastname
+	lastname = "Park"			#성은 박씨
 	
-pey = HousePark()
-pey.setName("KK")
+	def setName(self, name):		#이름을 입력 받고 fullname 을 생성
+		self.fullname = name + " " + self.lastname
+	
+	def travel(self, place):		#여행가고 싶은곳
+		self.travel_place = place
+
+	
+
+
+pey = HousePark()				# pey 의 클래스 생성
+pey.setName("pey")
+pey.travel("Paris, France")
+
+kw = HousePark()				#kw의 class생성
+kw.setName("Kw")
+kw.travel("London")
 
 print(pey.fullname)
-	
+print(pey.travel_place)	
+print(kw.fullname)
+print(kw.travel_place)
